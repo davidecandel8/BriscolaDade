3472573f66a7e5052165a06e56ede79d221186e9
davidecandel8 <davidecandela02@gmail.com>
2024-09-13 19:02:43 +0200

    continuo interfaccia

--- docs/app.js
+++ docs/app.js
@@ -1,6 +1,7 @@
 function showInsertOptions() {
     document.getElementById("home-section").style.display = "none";
     document.getElementById("insert-options-section").style.display = "flex";
+    document.getElementById("form-section").style.display = "none";
     document.getElementById("subtitle").textContent = "Seleziona il tipo di briscola della partita da inserire:";
     history.pushState({page: "insert-options"}, "Insert Options", "?page=insert-options");
 }
@@ -11,6 +12,7 @@ function showStats() {
 }
 
 function showForm(gameType) {
+    document.getElementById("home-section").style.display = "none";
     document.getElementById("insert-options-section").style.display = "none";
     document.getElementById("form-section").style.display = "block";
     document.getElementById("form-title").textContent = "Inserisci dati per " + gameType;
@@ -34,7 +36,7 @@ window.onpopstate = function(event) {
             document.getElementById("home-section").style.display = "flex";
             document.getElementById("insert-options-section").style.display = "none";
             document.getElementById("form-section").style.display = "none";
-            document.getElementById("subtitle").textContent = "Gestisci e visualizza le statistiche delle tue partite di Briscola";
+            document.getElementById("subtitle").textContent = "Tutte le partite e le statistiche di Briscola del 2024";
         }
     }
 };
