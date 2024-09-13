ddf51885fc3ae28efc9b2a751844339aa7c2616d
davidecandel8 <davidecandela02@gmail.com>
2024-09-14 00:46:33 +0200

    continuo interfaccia

--- docs/app.js
+++ docs/app.js
@@ -40,3 +40,27 @@ window.onpopstate = function(event) {
         }
     }
 };
+
+// Mostra il form specifico per "Briscola a 2"
+function showForm(gameType) {
+    document.getElementById("home-section").style.display = "none";
+    document.getElementById("insert-options-section").style.display = "none";
+    document.getElementById("form-section").style.display = "block";
+    document.getElementById("form-title").textContent = "Inserisci dati per " + gameType;
+    document.getElementById("subtitle").textContent = "Inserisci i dati della partita";
+    document.getElementById("game-date").valueAsDate = new Date(); // Imposta la data corrente
+    history.pushState({ page: "form" }, "Form", "?page=form");
+}
+
+// Funzione per gestire l'aggiornamento dei punti
+function updatePoints(inputField) {
+    const teamPointsInput = document.getElementById('team_points');
+    const opponentPointsInput = document.getElementById('opponent_points');
+
+    if (inputField.id === 'team_points' && teamPointsInput.value) {
+        opponentPointsInput.value = 120 - teamPointsInput.value;
+    } else if (inputField.id === 'opponent_points' && opponentPointsInput.value) {
+        teamPointsInput.value = 120 - opponentPointsInput.value;
+    }
+}
+
